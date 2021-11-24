49a33865c01ea6183eb74b4be6b8080945bffc3b
SodxOne <anthonyprorok@hotmail.com>
2021-11-24 21:01:47 +0700

    full code

--- contracts/Donation.sol
+++ contracts/Donation.sol
@@ -2,6 +2,29 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Donation {
+  address payable public owner;
+  address[] public donators;
+
   constructor() public {
+    owner = payable(msg.sender);
+  }
+  //Функция перевода пожертований
+  function gatDonation() public payable{
+    require(msg.value >= .0001 ether);
+    donators.push(msg.sender);
+  }
+
+  //Вывод на счет владельца
+  function transferToOwner() external{
+    require(msg.sender == owner);
+    owner.transfer(address(this).balance);
+  }
+//Возращает кто задонатил
+  function getDonators() public view returns(address[] memory){
+    return donators;
+  }
+  //Баланс нашего контракта
+  function balanceCont() public returns(uint256){
+    return address(this).balance;
   }
 }
