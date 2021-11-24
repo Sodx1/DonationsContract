9cdd73351d64fbd1ec5d186babf3de5cdf4088a2
SodxOne <anthonyprorok@hotmail.com>
2021-11-24 21:07:47 +0700

    Full Code Donat

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const Donation = artifacts.require("Donation");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Donation);
 };
